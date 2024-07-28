25023fd1529c2916be4210c71c4401cd00089f86
makhmedov.alikhan@gmail.com <dart.vader.develop@mail.ru>
2024-07-28 12:25:34 +0500

    3Day

--- src/main/java/kz/dara/intensivecourse/controller/EmployeeController.java
+++ src/main/java/kz/dara/intensivecourse/controller/EmployeeController.java
@@ -3,17 +3,18 @@ package kz.dara.intensivecourse.controller;
 import kz.dara.intensivecourse.model.Employee;
 import kz.dara.intensivecourse.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("api/")
 public class EmployeeController {
-    
+
     @Autowired
     private final EmployeeRepository employeeRepository;
 
@@ -22,17 +23,47 @@ public class EmployeeController {
     }
 
     @GetMapping("/employee/{id}")
-    public Optional<Employee> getEmployees(@PathVariable Long id){
+    public Optional<Employee> getEmployee(@PathVariable Long id) {
         Optional<Employee> employee = employeeRepository.findById(id);
         return employee;
     }
 
     @GetMapping("/employees")
-    public Employee createEmployee() {
-        Employee employee = new Employee();
-        employee.setFirstName("Saule");
-        employee.setLastName("Zhetpisbaeva");
-        employee.setEmailId("Saule@mail.ru");
+    public List<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
+
+    @PutMapping("/employee/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(NullPointerException::new);
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id).orElseThrow(NullPointerException::new);
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+
+
+//    @GetMapping("/employee/1")
+//    public Employee createEmployee() {
+//        Employee employee = new Employee();
+//        employee.setFirstName("Saule");
+//        employee.setLastName("Zhetpisbaeva");
+//        employee.setEmailId("Saule@mail.ru");
+//        return employeeRepository.save(employee);
+//    }
 }
